5e5dc6f7865bc67c7d6eb78cbca3bcf0e83d7f64
David Witherspoon <david.witherspoon@kaazing.com>
2017-02-16 16:46:21 -0800

    Initial working experiment with http2 over ws

---
+++ integration-test/http2-ws-itest.js
@@ -0,0 +1,20 @@
+var expect = require('chai').expect;
+var http2ws = require('../lib/http2-ws');
+
+describe('http2-cache', function () {
+
+    // TODO consider doing browser testing
+    // if (browserConfig) {
+    //     browserConfig.origin('http://localhost:8080').addResource("http://chaijs.com/chai.js");
+    // }
+
+    it('http2.get.over.ws', function (done) {
+        // DPW TODO proper handling of URL
+        http2ws.get({'host': 'localhost', 'port': 8080, 'transport':'ws://localhost:8080/echo'}, function(response){
+            console.log(response);
+            done();
+        })
+    });
+
+});
+
