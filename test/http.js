0cd16d0f476c6ab91cbcac560b34fc665dd69293
David Witherspoon <david.witherspoon@kaazing.com>
2017-12-08 13:09:08 -0800

    Added support for accept-encoding gzip  (#15)
    Added support for accept-encoding gzip

--- test/http.js
+++ test/http.js
@@ -10,6 +10,7 @@ var http2 = require('../lib/http');
 var https = require('https');
 var http = require('http');
 var websocket = require('websocket-stream');
+var pako = require('pako');
 
 var serverOptions = {
   key: fs.readFileSync(path.join(__dirname, '../example/localhost.key')),
@@ -95,6 +96,43 @@ describe('http.js', function() {
       });
     });
   });
+
+  describe('should accept-encoding gzip', function() {
+      it('does a request and gets a response with gzip encoding', function (done) {
+          var path = '/x';
+          var message = 'Hello world';
+
+          var compressedMessage = pako.gzip(message);
+          compressedMessage = Buffer.from(compressedMessage.buffer);
+          var server = http2.createServer(serverOptions, function (request, response) {
+              expect(request.url).to.equal(path);
+              response.setHeader('content-encoding', 'gzip');
+              var chunk1 = Buffer.from(compressedMessage, 0, 15);
+              response.write(chunk1);
+              response.write(Buffer.from(compressedMessage, 0, 0));
+              var chunk2 = Buffer.from(compressedMessage, 15);
+              response.write(chunk2);
+              response.end();
+          });
+
+          server.listen(1244, function () {
+              var options = url.parse('https://localhost:1244' + path);
+              options.key = agentOptions.key;
+              options.ca = agentOptions.ca;
+              options.rejectUnauthorized = true;
+
+              http2.globalAgent = new http2.Agent({log: util.clientLog});
+              http2.get(options, function (response) {
+                  response.on('data', function (data) {
+                      expect(data.toString()).to.equal(message);
+                      server.close();
+                      done();
+                  });
+              });
+          });
+      });
+  });
+
   describe('Agent', function() {
     describe('property `maxSockets`', function() {
       it('should be a proxy for the backing HTTPS agent\'s `maxSockets` property', function() {
@@ -106,6 +144,7 @@ describe('http.js', function() {
         expect(backingAgent.maxSockets).to.be.equal(newMaxSockets);
       });
     });
+
     describe('method `request(options, [callback])`', function() {
       it('should use a new agent for request-specific TLS settings', function(done) {
         var path = '/x';
